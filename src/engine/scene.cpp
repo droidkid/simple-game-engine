a8778f5911af13bebefe87ce4bf2b59bbcd9d69d
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2018-04-06 01:56:19 +0530

    adding scene.cpp

---
+++ src/engine/scene.cpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "engine/scene.h"
+#include "game_constants.h"
+
+using namespace GameConstants;
+
+Scene::Scene() {
+    curIdx = 0;
+    gameObjects = new GameObject*[MAX_GAME_OBJECTS];
+    for ( int i=0; i<MAX_GAME_OBJECTS; i++ ) {
+        gameObjects[i] = NULL;
+    }
+}
+
+void Scene::update(Input *input) {
+    GameObject *curObject;
+    for ( int i=0; i<MAX_GAME_OBJECTS; i++ ) {
+        curObject = gameObjects[i];
+        if ( curObject == NULL || curObject->canBeDestroyed() ) {
+            continue;
+        }
+        curObject->update ( input );
+    }
+}
+
+void Scene::addGameObject(GameObject *object) {
+    int tries = MAX_GAME_OBJECTS;
+    GameObject *curObject;
+
+    while ( tries-- ) {
+        if ( curIdx >= MAX_GAME_OBJECTS ) {
+            curIdx = 0;
+        }
+        curObject = gameObjects[curIdx];
+        if ( curObject == NULL ) {
+            gameObjects[curIdx++] = object;
+            return;
+
+        } else if ( curObject->canBeDestroyed() ) {
+            // TODO(chesetti): Call curObject destructor
+            gameObjects[curIdx++] = object;
+            return;
+        }
+        curIdx++;
+    }
+    // THROW ERROR HERE.
+}
+
+void Scene::draw(Canvas *canvas) {
+    canvas->clearScreen();
+    GameObject *curObject;
+    for ( int i=0; i<MAX_GAME_OBJECTS; i++ ) {
+        curObject = gameObjects[i];
+        if ( curObject == NULL || curObject->canBeDestroyed() ) {
+            continue;
+        }
+        curObject->draw ( canvas );
+    }
+    canvas->renderScreen();
+}
